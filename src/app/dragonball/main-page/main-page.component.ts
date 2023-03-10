1072dd1b6197bc41443ff61760df9349eb093eb6
El-Litios <carlosguzmanrojas00@gmail.com>
2023-03-09 22:42:43 -0300

    Funcion para agregar pj al arreglo

--- src/app/dragonball/main-page/main-page.component.ts
+++ src/app/dragonball/main-page/main-page.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dragonball.interface';
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
 
-  agregar(){
-    console.log('aa');
-    
+  personajes: Personaje[] = [
+    {
+      nombre: 'Cell',
+      poder: 200000
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }else{
+      this.personajes.push(this.nuevo);
+      this.nuevo = {
+        nombre: '',
+        poder: 0
+      }
+    }
   }
 }
